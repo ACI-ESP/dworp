2aef78941f113d228311d71a31c567ca67da347f
Cash Costello <cash.costello@jhuapl.edu>
2018-05-07 10:38:28 -0400

    removed unnecessary code

--- dworp/plot.py
+++ dworp/plot.py
@@ -18,16 +18,5 @@ class PlotPauseObserver(PauseObserver):
     def __init__(self, delay, start=False, stop=False):
         super().__init__(delay, start, stop)
 
-    def start(self, current_time, agents, env):
-        if self.start_flag:
-            self.pause()
-
-    def step(self, time, agents, env):
-        self.pause()
-
-    def done(self, agents, env):
-        if self.stop_flag:
-            self.pause()
-
     def pause(self):
         plt.pause(self.delay)
